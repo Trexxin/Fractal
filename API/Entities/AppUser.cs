18a25e4f1a81ea4376daf79949b81e0691600d58
Trexxin <Charlesharris2322@gmail.com>
2022-07-05 17:23:07 -0500

    Implmented better password storage

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,7 @@ namespace API.Entities
         // Primary key
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
