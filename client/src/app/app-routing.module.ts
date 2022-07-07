47de4546add09fe243176527bb75170c42a6b168
Trexxin <Charlesharris2322@gmail.com>
2022-07-07 17:40:19 -0500

    Implemented route guarding for nav links

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowseComponent } from './browse/browse.component';
+import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -8,11 +9,18 @@ import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent},
-  {path: 'memebers/:id', component: MemberDetailComponent},
-  {path: 'browse', component: BrowseComponent},
-  {path: 'messages', component: MessagesComponent},
-  {path: '**', component: HomeComponent, pathMatch: 'full'}
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component: MemberListComponent},
+      {path: 'memebers/:id', component: MemberDetailComponent},
+      {path: 'browse', component: BrowseComponent},
+      {path: 'messages', component: MessagesComponent},
+      {path: '**', component: HomeComponent, pathMatch: 'full'}
+    ]
+  }
 ];
 
 @NgModule({
