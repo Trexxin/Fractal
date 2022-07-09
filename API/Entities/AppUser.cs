af4eeebecfbe95a95e6284865ca604e715e4a8f6
Trexxin <Charlesharris2322@gmail.com>
2022-07-08 19:12:53 -0500

    Exteded data in user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,5 +7,14 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Registered { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests {get; set; }
+        public string City { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
