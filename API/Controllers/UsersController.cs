153be88503b38e5cd36f4c5f49fed94ea7150979
Trexxin <Charlesharris2322@gmail.com>
2022-07-10 13:31:26 -0500

    Refactored code to use the userRepository

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,24 +15,24 @@ namespace API.Controllers
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository)
         {
-            _context = context;
+            _userRepository = userRepository;
         }
 
         // Endpoint to get all users in database
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
         {
-            return await _context.Users.ToListAsync();
+            return Ok(await _userRepository.GetUsersAsync());
         }
 
         // Endpoint to get an individual user by their id
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUsers(int id) 
+        [HttpGet("{UserName}")]
+        public async Task<ActionResult<AppUser>> GetUsers(string UserName) 
         {
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetUserByUsernameAsync(UserName);
         }
     }
 }
