e2e0d6d841c6b25272da8d7096662a013693984b
Trexxin <Charlesharris2322@gmail.com>
2022-07-10 13:30:59 -0500

    Created MemberDto

---
+++ API/Dtos/MemberDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Dtos
+{
+    public class MemberDto
+    {
+        // Primary key
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Registered { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
