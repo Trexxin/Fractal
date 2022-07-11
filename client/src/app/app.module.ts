438f91042a58efd551c2e0c6c47f2bb3f58325f9
Trexxin <Charlesharris2322@gmail.com>
2022-07-11 10:43:23 -0500

    Implemented interceptor to grant authorization

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { MemberCardComponent } from './members/member-card/member-card.component
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }
+    { provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true }},
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,50 +1,21 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 
-//Causes a 401 error and I have yet to understand why. The following code seems to work but more investigation is needed as to why
-// Temporary way to get authorization
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-}
-
 @Injectable({
   providedIn: 'root'
 })
+
 export class MembersService {
   baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(userName: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + userName, httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + userName);
   }
 }
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class MembersService {
-//   baseUrl = environment.apiUrl;
- 
-//   httpOptions = {
-//     headers: new HttpHeaders({
-//       Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
-//     })
-//   };
-  
-//   constructor(private http: HttpClient) { }
- 
-//   getMembers() {
-//     return this.http.get<Member[]>(this.baseUrl + 'users', this.httpOptions);
-//   }
-//   getMember(username: string) {
-//     return this.http.get<Member>(this.baseUrl + 'user/' + username, this.httpOptions);
-//   }
-// }
