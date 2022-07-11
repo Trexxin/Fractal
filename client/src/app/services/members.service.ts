930f6afcff374e672beca2aab20babdcc003cb75
Trexxin <Charlesharris2322@gmail.com>
2022-07-10 21:30:32 -0500

    Temporary fix for a crtical bug

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 
+//Causes a 401 error and I have yet to understand why. The following code seems to work but more investigation is needed as to why
 // Temporary way to get authorization
 const httpOptions = {
   headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
   })
-
 }
+
 @Injectable({
   providedIn: 'root'
 })
@@ -25,3 +26,25 @@ export class MembersService {
     return this.http.get<Member>(this.baseUrl + 'users/' + userName, httpOptions);
   }
 }
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class MembersService {
+//   baseUrl = environment.apiUrl;
+ 
+//   httpOptions = {
+//     headers: new HttpHeaders({
+//       Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+//     })
+//   };
+  
+//   constructor(private http: HttpClient) { }
+ 
+//   getMembers() {
+//     return this.http.get<Member[]>(this.baseUrl + 'users', this.httpOptions);
+//   }
+//   getMember(username: string) {
+//     return this.http.get<Member>(this.baseUrl + 'user/' + username, this.httpOptions);
+//   }
+// }
