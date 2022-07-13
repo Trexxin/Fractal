86da5ca42ea2932f78c8830539e26fbb85b5dba3
Trexxin <Charlesharris2322@gmail.com>
2022-07-12 20:37:09 -0500

    Implemented client state storage

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 
@@ -9,17 +11,32 @@ import { Member } from '../models/member';
 
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
+
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(userName: string) {
+    const member = this.members.find(x => x.userName === userName);
+    if (member !== undefined) return of(member)
     return this.http.get<Member>(this.baseUrl + 'users/' + userName);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member; 
+      })
+    )
   }
 }
