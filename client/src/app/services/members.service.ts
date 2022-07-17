979f2f1ce7cda32c6ab7568a19317c443e5696cc
Trexxin <Charlesharris2322@gmail.com>
2022-07-17 00:09:42 -0500

    Made the client rember filter state

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 import { PaginatedResult } from '../models/pagination';
+import { User } from '../models/user';
 import { UserParams } from '../models/userParams';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +17,28 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
+  user: User;
+  userParams: UserParams;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+      this.userParams = new UserParams(user);
+    })
+   }
 
+   getUserParams() {
+    return this.userParams;
+   }
+
+   setUserParams(params: UserParams) {
+    this.userParams = params;
+   }
+
+   resetUserParams() {
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+   }
 
   private getPaginationHeaders(pageNumber: number, pageSize: number) {
     let params = new HttpParams();
